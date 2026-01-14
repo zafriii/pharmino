5f1c2e6dbdee7dee81dc9ef201835e055677a174
zafriii <nihazafar050@gmail.com>
2026-01-15 01:33:48 +0600

    changed option names

--- src/components/Admin/Sale/Create Sale/SalePanel.tsx
+++ src/components/Admin/Sale/Create Sale/SalePanel.tsx
@@ -1323,7 +1323,7 @@ const SalePanel: React.FC = () => {
                             : "bg-white text-gray-700 border-gray-300 hover:bg-gray-50"
                         }`}
                       >
-                        Strip
+                        Full
                       </button>
                     )}
                     <button
