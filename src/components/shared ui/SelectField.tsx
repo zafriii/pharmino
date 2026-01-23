b57a3f22e2f5e9ea63827c6480c0a7f2312a82b0
zafriii <nihazafar050@gmail.com>
2026-01-23 22:07:11 +0600

    new folder structure

--- src/components/shared ui/SelectField.tsx
+++ src/components/shared ui/SelectField.tsx
@@ -1,57 +1,57 @@
-import {
-  FormField,
-  FormItem,
-  FormLabel,
-  FormControl,
-  FormMessage,
-} from '@/components/ui/form';
+// import {
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormControl,
+//   FormMessage,
+// } from '@/components/ui/form';
 
-import {
-  Select,
-  SelectTrigger,
-  SelectValue,
-  SelectContent,
-  SelectItem,
-} from '@/components/ui/select';
+// import {
+//   Select,
+//   SelectTrigger,
+//   SelectValue,
+//   SelectContent,
+//   SelectItem,
+// } from '@/components/ui/select';
 
-interface Option {
-  value: string;
-  label: string;
-}
+// interface Option {
+//   value: string;
+//   label: string;
+// }
 
-interface Props {
-  name: any;
-  label: string;
-  options: Option[];
-  form: any;
-}
+// interface Props {
+//   name: any;
+//   label: string;
+//   options: Option[];
+//   form: any;
+// }
 
-export default function SelectField({ name, label, options, form }: Props) {
-  return (
-    <FormField
-      control={form.control}
-      name={name}
-      rules={{ required: `${label} is required` }}
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>{label}</FormLabel>
-          <FormControl>
-            <Select onValueChange={field.onChange} defaultValue={field.value}>
-              <SelectTrigger>
-                <SelectValue placeholder={`Select ${label}`} />
-              </SelectTrigger>
-              <SelectContent>
-                {options.map((opt) => (
-                  <SelectItem key={opt.value} value={opt.value}>
-                    {opt.label}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-  );
-}
+// export default function SelectField({ name, label, options, form }: Props) {
+//   return (
+//     <FormField
+//       control={form.control}
+//       name={name}
+//       rules={{ required: `${label} is required` }}
+//       render={({ field }) => (
+//         <FormItem>
+//           <FormLabel>{label}</FormLabel>
+//           <FormControl>
+//             <Select onValueChange={field.onChange} defaultValue={field.value}>
+//               <SelectTrigger>
+//                 <SelectValue placeholder={`Select ${label}`} />
+//               </SelectTrigger>
+//               <SelectContent>
+//                 {options.map((opt) => (
+//                   <SelectItem key={opt.value} value={opt.value}>
+//                     {opt.label}
+//                   </SelectItem>
+//                 ))}
+//               </SelectContent>
+//             </Select>
+//           </FormControl>
+//           <FormMessage />
+//         </FormItem>
+//       )}
+//     />
+//   );
+// }
